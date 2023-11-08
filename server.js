fd25126faf01056a8099b07c0618feea4008b909
rajeshavul2 <rajeshavul2@gmail.com>
2023-11-08 16:36:06 +0200

    Iteration2 done

---
+++ server.js
@@ -0,0 +1,36 @@
+const connectDB = require("./config/db");
+const express = require("express");
+
+// express app
+const app = express();
+connectDB();
+
+// Import the controllers
+const {
+  getBlogs,
+  createBlog,
+  getBlog,
+  deleteBlog,
+  putBlog,
+} = require("./controllers/blogController");
+
+// middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => res.send("API Running!"));
+// GET all blogs
+app.get("/api/blogs", getBlogs);
+// GET a single blog
+app.get("/api/blogs/:id", getBlog);
+// Add a new blog
+app.post("/api/blogs", createBlog);
+// Add a delete blog
+app.delete("/api/blogs/:id", deleteBlog);
+// Add a put blog
+app.put("/api/blogs/:id", putBlog);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
